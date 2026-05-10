44f4f6f64579699ec67e365afe311297ff91f424
Mariana Carreño <145129041+Marianacr06@users.noreply.github.com>
2026-05-10 18:43:39 -0500

    feat: agregar codigo sucio con Switch Statement y Arrow Code

--- src/main/java/com/empresa/pedidos/dominio/Cliente.java
+++ src/main/java/com/empresa/pedidos/dominio/Cliente.java
@@ -1,31 +1,16 @@
 package com.empresa.pedidos.dominio;
 
+package com.empresa.pedidos.dominio;
+
 public class Cliente {
     private boolean activo;
     private int score;
     private double limiteCredito;
 
-    public boolean isActivo() {
-        return activo;
-    }
-
-    public void setActivo(boolean activo) {
-        this.activo = activo;
-    }
-
-    public int getScore() {
-        return score;
-    }
-
-    public void setScore(int score) {
-        this.score = score;
-    }
-
-    public double getLimiteCredito() {
-        return limiteCredito;
-    }
-
-    public void setLimiteCredito(double limiteCredito) {
-        this.limiteCredito = limiteCredito;
-    }
+    public boolean isActivo() { return activo; }
+    public int getScore() { return score; }
+    public double getLimiteCredito() { return limiteCredito; }
+    public void setActivo(boolean activo) { this.activo = activo; }
+    public void setScore(int score) { this.score = score; }
+    public void setLimiteCredito(double limiteCredito) { this.limiteCredito = limiteCredito; }
 }
